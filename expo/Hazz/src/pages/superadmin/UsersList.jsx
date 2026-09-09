f009fe546a74d41d24068c46a6d123d7c446cfc2
droidDev-Ak <programcoder.ak@gmail.com>
2026-09-09 19:48:29 +0530

    fix: add missing EmployeeProfileSidecar import

--- expo/Hazz/src/pages/superadmin/UsersList.jsx
+++ expo/Hazz/src/pages/superadmin/UsersList.jsx
@@ -1,3 +1,4 @@
+import EmployeeProfileSidecar from '../../components/EmployeeProfileSidecar';
 import { useAuth } from '@clerk/react';
 import React, { useState, useEffect } from 'react';
 import SidebarLayout from '../../layouts/SidebarLayout';
